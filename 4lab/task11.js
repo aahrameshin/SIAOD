8879bd77a0b0a94e29a8ed1a4304da2b57a531d2
aahrameshin <73755680+aahrameshin@users.noreply.github.com>
2021-05-17 19:33:07 +0300

    Add files via upload

---
+++ 4lab/task11.js
@@ -0,0 +1,20 @@
+const Stack = require('./stack');
+
+function computeForm (Str)
+{
+    let stk= new Stack.Stack()
+    let str=""
+    for(let i=0;i<Str.length;i++){
+        stk.push(Str[i])
+    }
+    for (let i=0;i<Str.length;i++){
+        str=stk.pop()+str
+    }
+    try{
+        eval(str)
+    }
+    catch (err){
+        console.log(false)
+    }
+    console.log(true)
+}
